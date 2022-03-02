87c1d75fb24d0ee945117ef8f961293d631d7b6d
Jean Alves <jean.donizete.alves@gmail.com>
2022-03-02 02:48:31 -0300

    feat: inserindo metodo para salvar curso

--- src/app/pages/courses/services/courses.service.ts
+++ src/app/pages/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Course } from './../model/course.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +15,9 @@ export class CoursesService {
 
   findAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses');
+  }
+
+  saveCourse(courseId: number, changes: Partial<Course>): Observable<Course> {
+    return this.http.put<Course>(`/api/courses/${courseId}`, changes);
   }
 }
