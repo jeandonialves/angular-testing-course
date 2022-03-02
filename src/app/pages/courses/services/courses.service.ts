bf860c8624f9c605425c55f28bfd5c802cbff112
Jean Alves <jean.donizete.alves@gmail.com>
2022-03-02 02:34:40 -0300

    feat: inserindo metodo para buscar curso por id

--- src/app/pages/courses/services/courses.service.ts
+++ src/app/pages/courses/services/courses.service.ts
@@ -9,6 +9,10 @@ import { map, Observable } from 'rxjs';
 export class CoursesService {
   constructor(private http: HttpClient) {}
 
+  findCourseById(id: number): Observable<Course> {
+    return this.http.get<Course>('/api/courses/' + id);
+  }
+
   findAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses');
   }
