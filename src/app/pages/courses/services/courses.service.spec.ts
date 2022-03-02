980bd3ae5b3d83fdbf0a4c323070959fbd925a6d
Jean Alves <jean.donizete.alves@gmail.com>
2022-03-02 02:19:50 -0300

    test: criando teste para all courses

--- src/app/pages/courses/services/courses.service.spec.ts
+++ src/app/pages/courses/services/courses.service.spec.ts
@@ -1,16 +1,40 @@
+import { Course } from './../model/course.model';
+import { COURSES } from './../../../../testing/mock/db.data';
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { CoursesService } from './courses.service';
 
-describe('CoursesService', () => {
+fdescribe('CoursesService', () => {
   let service: CoursesService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve all courses', () => {
+    service.findAllCourses().subscribe((courses: Course[]) => {
+      const course = courses.find((course) => course.id == 1);
+
+      expect(course?.title).toBe('Angular Testing Course');
+    });
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(Object.values(COURSES));
   });
 });
